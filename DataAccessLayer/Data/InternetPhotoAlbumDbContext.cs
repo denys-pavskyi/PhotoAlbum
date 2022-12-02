1f15cf53965d233b407d00f9750fd398de6be3a7
denys_pavskyi <den.pavski@gmail.com>
2022-12-02 04:22:33 +0200

    Created DbContext, configured connection string and configured connection to db in Program.cs

---
+++ DataAccessLayer/Data/InternetPhotoAlbumDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Data
+{
+    public class InternetPhotoAlbumDbContext: DbContext
+    {
+        public InternetPhotoAlbumDbContext(DbContextOptions<InternetPhotoAlbumDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<AlbumPhoto> AlbumPhotos { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<PhotoRating> PhotosRating { get; set; }
+        public DbSet<PhotoTag> PhotoTags { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+
+
+    }
+}
