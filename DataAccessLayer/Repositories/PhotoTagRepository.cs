fdaa0478602d37368d53f0ef5ff0e69b656e6d5d
denys_pavskyi <den.pavski@gmail.com>
2022-12-03 05:25:13 +0200

    Changed PhotoTag repository

--- DataAccessLayer/Repositories/PhotoTagRepository.cs
+++ DataAccessLayer/Repositories/PhotoTagRepository.cs
@@ -1,5 +1,7 @@
 ﻿using DataAccessLayer.Data;
+using DataAccessLayer.Entities;
 using DataAccessLayer.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,62 @@ namespace DataAccessLayer.Repositories
         public PhotoTagRepository(InternetPhotoAlbumDbContext internetPhotoAlbumDbContext)
         {
             _context = internetPhotoAlbumDbContext;
+        }
+
+        public async Task AddAsync(PhotoTag entity)
+        {
+            await _context.PhotoTags.AddAsync(entity);
+        }
+
+        public void Delete(PhotoTag entity)
+        {
+            _context.PhotoTags.Remove(entity);
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var PhotoTag = await GetByIdWithDetailsAsync(id);
+            _context.PhotoTags.Remove(PhotoTag);
+        }
+
+        public async Task<IEnumerable<PhotoTag>> GetAllAsync()
+        {
+            return await _context.PhotoTags.ToListAsync();
+        }
+
+
+
+        public async Task<PhotoTag> GetByIdAsync(int id)
+        {
+            return await _context.PhotoTags.FindAsync(id);
+        }
+
+        public void Update(PhotoTag entity)
+        {
+            _context.PhotoTags.Update(entity);
+
+        }
+
+        public async Task<PhotoTag> GetByIdWithDetailsAsync(int id)
+        {
+            return await _context.PhotoTags
+                .Include(x => x.Photo)
+                .Include(x => x.Tag)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+        }
+
+        public async Task<IEnumerable<PhotoTag>> GetAllWithDetailsAsync()
+        {
+            return await _context.PhotoTags
+                .Include(x => x.Photo)
+                .Include(x => x.Tag)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<PhotoTag>> GetAllWithNoTrackingAsync()
+        {
+            return await _context.PhotoTags.AsNoTracking().ToListAsync();
         }
     }
 }
